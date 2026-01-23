8ea5be480b2d8fc190fe9d92013684fec95bab62
Rifat <172rifat722@gmail.com>
2026-01-23 23:51:35 +0500

    add - inertia vue

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Привет, {{ user }}!</h1>
+  <p>Добро пожаловать в Inertia приложение.</p>
+</template>
+
+<script setup>
+defineProps({ user: String })
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 import './bootstrap';
+import '../css/app.css'; // Импорт стилей прямо в JS
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
+createInertiaApp({
+    title: (title) => `${title} - My App`,
+    // Заменяем require на динамический импорт Vite
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el);
+    },
+});
