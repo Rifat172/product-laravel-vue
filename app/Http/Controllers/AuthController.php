6f4d411ab5b0ebfdce6c393a9d134858bb03f66c
Rifat <172rifat722@gmail.com>
2026-01-23 13:14:59 +0500

    update AuthController.php - fix error

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'passwordr' => Hash::make($request->password),
+            'password' => Hash::make($request->password),
         ]);
 
         return response()->json(['message' => 'Registered', 'user' => $user]);
